07ee98fb868a5cec762a8015f5aa81bc0b9e03dd
AlansidneyJunior <alansidney.silva21@gmail.com>
2025-01-09 10:57:47 -0300

    Adicionando mensagens.

--- projeto/src/main/java/br/com/tomus/projeto/managedBeans/AlunoMB.java
+++ projeto/src/main/java/br/com/tomus/projeto/managedBeans/AlunoMB.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,19 +26,32 @@ public class AlunoMB {
     private List<Aluno> alunos = new ArrayList<>();
 
     public String save() {
-    	if(aluno.getId() == null) {
-    		alunoDao.save(aluno);
-    		alunos.add(aluno);
-    	} else {
-    		alunoDao.update(aluno);
-    	}
+    	try {
+    		if(aluno.getId() == null) {
+        		alunoDao.save(aluno);
+        		alunos.add(aluno);
+        		
+        		FacesContext.getCurrentInstance().addMessage("", new FacesMessage("SUCESSO: Aluno cadastrado.", "Aluno cadastrado com Sucesso."));
+        	} else {
+        		alunoDao.update(aluno);
+        		FacesContext.getCurrentInstance().addMessage("", new FacesMessage("SUCESSO: Aluno atualizado.", "Aluno atualizado com Sucesso."));
+        	}
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR,"ERRO: Aluno NÃO foi cadastrado.", "ERROR: Aluno NÃO foi cadastrado."));
+		}
     	updateAlunoList();
     	clearForm();
         return null;
     }
 
     public String remove() {
-        alunoDao.remove(aluno.getId());
+    	try {
+    		alunoDao.remove(aluno.getId());
+    		FacesContext.getCurrentInstance().addMessage("", new FacesMessage("SUCESSO: Aluno REMOVIDO.", "SUCESSO: Aluno REMOVIDO."));
+    	} catch (Exception e) {
+    		FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR,"ERRO: Aluno NÃO foi REMOVIDO.", "ERROR: Aluno NÃO foi REMOVIDO."));
+		}
+ 
         updateAlunoList();
         clearForm();
         return null;
@@ -45,6 +60,7 @@ public class AlunoMB {
     public String edit(Aluno selectedAluno) {
     	
     	this.aluno = selectedAluno;
+    	FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_WARN,"MODO DE EDIÇÃO: Atualize os dados do aluno.", "MODO DE EDIÇÃO: Atualize os dados do aluno."));
     	return null;
     }
     
